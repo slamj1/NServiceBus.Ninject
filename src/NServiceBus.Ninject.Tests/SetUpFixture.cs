64232ae149f296da5834304869e568099c53b3a9
Simon Cropp <simon.cropp@gmail.com>
2014-09-17 20:48:36 +1000

    fix test

--- src/NServiceBus.Ninject.Tests/SetUpFixture.cs
+++ src/NServiceBus.Ninject.Tests/SetUpFixture.cs
@@ -1,4 +1,7 @@
-﻿using NServiceBus.ContainerTests;
+﻿using Ninject;
+using Ninject.Extensions.ContextPreservation;
+using Ninject.Extensions.NamedScope;
+using NServiceBus.ContainerTests;
 using NServiceBus.ObjectBuilder.Ninject;
 using NUnit.Framework;
 
@@ -8,7 +11,8 @@ public class SetUpFixture
     [SetUp]
     public void Setup()
     {
-        TestContainerBuilder.ConstructBuilder = () => new NinjectObjectBuilder();
+        TestContainerBuilder.ConstructBuilder = () => new NinjectObjectBuilder(new StandardKernel(new NinjectSettings {LoadExtensions = false},
+                                                                new ContextPreservationModule(), new NamedScopeModule()));
     }
 
 }
