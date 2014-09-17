bbe17858daa9655679ce4757e4a49a05ff0d7601
Simon Cropp <simon.cropp@gmail.com>
2014-09-17 20:44:21 +1000

    add unit tests

---
+++ src/NServiceBus.Ninject.Tests/SetUpFixture.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus.ContainerTests;
+using NServiceBus.ObjectBuilder.Ninject;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class SetUpFixture
+{
+    [SetUp]
+    public void Setup()
+    {
+        TestContainerBuilder.ConstructBuilder = () => new NinjectObjectBuilder();
+    }
+
+}
